12d7e76bb0054c27be2aecadce9c0f35076c2692
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2026-06-20 08:27:43 +0200

    lekcja 02 do przodu

--- src/test/java/com/dydko/steps/TablesLesson02Steps.java
+++ src/test/java/com/dydko/steps/TablesLesson02Steps.java
@@ -1,14 +1,21 @@
 package com.dydko.steps;
 
+import com.dydko.pages.LoginLesson02Page;
 import com.dydko.pages.TablesLesson02Page;
 import io.cucumber.java.en.Given;
 
 public class TablesLesson02Steps {
 
     private final TablesLesson02Page page = new TablesLesson02Page();
+    private final LoginLesson02Page  loginPage = new LoginLesson02Page();
 
     @Given("user opens tables page lesson 02")
     public void userOpensTableLesson02Page(){
         page.openPage();
+    }
+
+    @Given("user opens logins page lesson 02")
+    public void userOpensLoginsLesson02Page(){
+        loginPage.openPage();
     }
 }
--- src/test/resources/features/tutorials/users_lesson_01.feature
+++ src/test/resources/features/tutorials/users_lesson_01.feature
@@ -3,8 +3,9 @@ Feature: Users table
   Scenario: Read first user lesson 01
     Given user opens tables page lesson 01
 
-
-  Scenario: Read first user lesson 02
+  Scenario: User opens tables lesson 02
     Given user opens tables page lesson 02
 
+  Scenario: User opens logins lesson 02
+    Given user opens logins page lesson 02
 

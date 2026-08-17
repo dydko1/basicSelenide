f029cf11dc4d4ac7ff34320df074b55dfa3a7d15
Miroslaw Dyduch <miroslaw.dyduch@gmail.com>
2026-08-17 19:45:00 +0200

    lekcja 12 do przodu

--- src/test/java/com/dydko/steps/UserValidationLesson12Steps.java
+++ src/test/java/com/dydko/steps/UserValidationLesson12Steps.java
@@ -2,22 +2,18 @@ package com.dydko.steps;
 
 import com.dydko.models.User;
 import com.dydko.testsupport.ScenarioContext;
-import com.dydko.testsupport.TestContext;
 import com.dydko.testsupport.TestContextKey;
 import io.cucumber.java.en.Then;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserValidationLesson12Steps {
 
     @Then("loaded user should have first name {string}")
-    public void loadedUserShouldHaveFirstName(
-            String expectedFirstName) {
+    public void loadedUserShouldHaveFirstName(String expectedFirstName) {
         User user =
                 ScenarioContext
                         .get()
                         .get(TestContextKey.USER);
-
         assertThat(user.firstName())
                 .isEqualTo(expectedFirstName);
     }
